00db9f9da77242d62da3d80584a4181591407e49
DiegoAlbarracin <diegoalbarracin0405@gmail.com>
2023-07-29 11:39:21 -0500

    Creating the logic of the function 'summOfTwoNumbers' and applying the functionality to the 'main' function

--- Experimenting.cpp
+++ Experimenting.cpp
@@ -4,13 +4,17 @@ int summofTwoNumbers(int number1, int number2);
 
 
 int main(){
-    cout << "HOLAAAA" << endl;
-    cout << "HIIIIIIIIIIIII" <<endl;
+    int num1, num2;
+    cout << "Enter the number 1:" << endl;
+    cin >> num1;
+    cout << "Enter the number 2:" <<endl;
+    cin >> num2;
+    cout << "The result is: " << summofTwoNumbers(num1,num2) << endl;
     return 0; 
 }
 
 int summofTwoNumbers(int number1, int number2){
-
+    return number1+number2;
 }
 
 int subtractionOfTwoNumbers(int number1, int number2){
