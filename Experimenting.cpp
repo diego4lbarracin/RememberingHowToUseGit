8287d8af18b0d27451256b777aaa91ee35ec9d3b
DiegoAlbarracin <diegoalbarracin0405@gmail.com>
2023-07-29 11:29:16 -0500

    Creating the frame of the code

--- Experimenting.cpp
+++ Experimenting.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+int summofTwoNumbers(int number1, int number2);
+
 
 int main(){
     cout << "HOLAAAA" << endl;
     cout << "HIIIIIIIIIIIII" <<endl;
     return 0; 
+}
+
+int summofTwoNumbers(int number1, int number2){
+
+}
+
+int subtractionOfTwoNumbers(int number1, int number2){
+    
 }
